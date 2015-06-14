7e711aa0c5dfcbc06f827520418aab7c32d72c56
Shaun Donnelly <shaundonnelly@gmail.com>
2015-06-14 09:30:27 +0100

    eba eba

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ app.listen(app.get('port'), function() {
     console.log('Server running on port ' + app.get('port'));
 });
 
-app.use('/', express.static('public'));
+app.use('/', express.static(__dirname + '/public'));
 
 // GET /unlock.
 app.get('/unlock', function(req, res) {
